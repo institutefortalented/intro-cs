52d791f23a7fd4174aa356ee75f2b7c3c3f36364
Christina Shao <christina.shao@berkeley.edu>
2020-06-26 01:13:01 -0700

    add java art gallery

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -14,6 +14,21 @@ I run a weekly Web Design Club for high schoolers -- if you're interested, let m
     switch (course) {
         case 'java':
             $('#course').html('Intro to CS - Java');
+            let gallery = ``;
+            for (let i = 0; i < javaGallery.length; i+=3) {
+                gallery += `<tr>`;
+                for (let j = 0; j < 3 && i+j < javaGallery.length; j++) {
+                    let entry = javaGallery[i+j];
+                    gallery += `<td class="entry">
+                        <img src="${entry.img}"/>
+                        <div class="entry-title">${entry.title}</div>
+                        <div class="entry-name">by ${entry.name ? entry.name : 'Anonymous'}</div>
+                        <div class="entry-caption">${entry.caption}</div>
+                    </td>`;
+                }
+                gallery += `</tr>`;
+            }
+            $('#gallery').html(gallery);
             break;
         default:
             location.href = 'index.html';
---
+++ scripts/gallery/gallery_java.js
@@ -0,0 +1,55 @@
+let javaGallery = [
+    {
+        img: 'gallery/java/9.jpg',
+        title: "Weird Sun Thing",
+        name: 'Ella',
+        caption: `This is an "orange" (even though it is obviously yellow) sun thing.  Nice eh?`
+    },
+    {
+        img: 'gallery/java/8.jpg',
+        title: "An Out of the Lines Star",
+        name: 'Ella',
+        caption: "This is a blue star that is tilted and has an empty colored shadow.  Perhaps it is doing cartwheels."
+    },
+    {
+        img: 'gallery/java/7.jpg',
+        title: "A 12-16-20 Right Triangle",
+        name: 'Ella',
+        caption: `This is a right triangle who's sides are 12, 16, and 20.  It is a pale red that is called "pink" in java.  It says hi.`
+    },
+    {
+        img: 'gallery/java/6.png',
+        title: "Christmas Tree",
+        name: 'Michael',
+        caption: ''
+    },
+    {
+        img: 'gallery/java/5.png',
+        title: "Territorial Disputes",
+        name: 'Alan',
+        caption: "Beep boop. This artwork was created with the help of the magical world of computers, so there is no true meaning to it. You decide what it is."
+    },
+    {
+        img: 'gallery/java/4.png',
+        title: "Valley",
+        name: 'Alan',
+        caption: "Beep boop. This artwork was created with the help of Math.random(), so there is no true meaning to it. You decide what it is."
+    },
+    {
+        img: 'gallery/java/3.png',
+        title: "Empty Horizon",
+        name: 'Alan',
+        caption: "Beep boop. This artwork was created with the help of randomization, so there is no true meaning to it. You decide what it is."
+    },
+    {
+        img: 'gallery/java/2.png',
+        title: "Angaman",
+        name: 'Alan',
+        caption: "Beep boop. This artwork was created with the help of robots, so there is no true meaning to it. You decide what it is."
+    },
+    {
+        img: 'gallery/java/1.png',
+        title: "Triangle",
+        caption: "It took me an hour to draw a triangle :)<br>Totally worth it though V(^_^)V"
+    }
+];
