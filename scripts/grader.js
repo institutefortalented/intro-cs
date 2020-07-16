1e59885d5218f1f2f9ee35a5df99b2dc89b47741
Christina Shao <christina.shao@berkeley.edu>
2020-07-16 15:15:34 -0700

    release hw13 pythonE grader

--- scripts/grader.js
+++ scripts/grader.js
@@ -35,6 +35,7 @@ I run a weekly Web Design Club for high schoolers -- if you're interested, let m
             $('#assignments').html(`
                 <button onclick="gallery('pythonE')" class="${glryButtonClass}">Art Gallery</button>
                 <div style="height: 25px;"></div>
+                <button onclick="grader('hw13')" class="${hwButtonClass}">Homework 13</button>
                 <button onclick="grader('hw12')" class="${hwButtonClass}">Homework 12</button>
                 <button onclick="grader('hw11')" class="${hwButtonClass}">Homework 11</button>
                 <button onclick="grader('hw10')" class="${hwButtonClass}">Homework 10</button>
@@ -224,6 +225,7 @@ function grade(code, hw) {
             else if (hw == 'hw10') cases = hw10_e_cases;
             else if (hw == 'hw11') cases = hw11_e_cases;
             else if (hw == 'hw12') cases = hw12_e_cases;
+            else if (hw == 'hw13') cases = hw13_e_cases;
             else dialog(hwErrMessage);
             let callback = results;
             for (const num in cases) {
