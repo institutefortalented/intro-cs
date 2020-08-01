6d202cd8b0c435e02553281f74bd31eaab407819
Christina Shao <christina.shao@berkeley.edu>
2020-07-31 18:11:33 -0700

    release hw17 pythonM grader

--- scripts/grader.js
+++ scripts/grader.js
@@ -54,6 +54,7 @@ I run a weekly Web Design Club for high schoolers -- if you're interested, let m
         case 'pythonM':
             $('#course').html('Intro to CS - Python (M)');
             $('#assignments').html(`
+                <button onclick="grader('hw17')" class="${hwButtonClass}">Homework 17</button>
                 <button onclick="grader('hw16')" class="${hwButtonClass}">Homework 16</button>
                 <button onclick="grader('hw15')" class="${hwButtonClass}">Homework 15</button>
                 <button onclick="grader('hw14')" class="${hwButtonClass}">Homework 14</button>
@@ -182,6 +183,7 @@ function grade(code, hw) {
                 else if (hw == 'hw14') cases = hw14_m_cases;
                 else if (hw == 'hw15') cases = hw15_m_cases;
                 else if (hw == 'hw16') cases = hw16_m_cases;
+                else if (hw == 'hw17') cases = hw17_m_cases;
                 else dialog(hwErrMessage);
                 let callback = results;
                 for (const num in cases) {
