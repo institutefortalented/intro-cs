c507edd1435689e6544ef7027ea5a34eedf35d61
Christina Shao <christina.shao@berkeley.edu>
2020-08-03 01:42:29 -0700

    update pythonE gallery

--- scripts/gallery/gallery_pythonE.js
+++ scripts/gallery/gallery_pythonE.js
@@ -1,5 +1,11 @@
 let pythonEGallery = [
     {
+        img: 'gallery/pythonE/5.png',
+        title: 'One Gallant and Classy Rose',
+        name: 'Anna',
+        caption: "This took me quite some time. . . but anyway, this happens to your rose if you practice, and do enough turtle graphics. Because if you do enough, Python will be happy instead of mad and will give you a happier rose if you like. If you don't do Python enough it will give you a rutted cupcake. . ."
+    },
+    {
         img: 'gallery/pythonE/4.png',
         title: "A Very Rutted Cupcake",
         name: 'Anna',
